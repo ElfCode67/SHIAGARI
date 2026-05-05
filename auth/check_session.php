1b69b7c8bff4f3a245343a7b5cd762bc9beec0f7
ElfCode67 <balmesgenrique27@gmail.com>
2026-05-05 22:36:33 +0800

    .

--- auth/check_session.php
+++ auth/check_session.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once '../config/database.php');
+require_once '../config/database.php';
 
 if (isset($_SESSION['user_id'])) {
     echo json_encode([
